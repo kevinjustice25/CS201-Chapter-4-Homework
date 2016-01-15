17e11c5361820aa158dbc32aea3e9b00ecaa8de1
kevinjustice25 <kevinjustice25@gmail.com>
2016-01-14 20:42:47 -0600

    Create Kevin Justice Week3Day4 Homework #1.py

---
+++ Kevin Justice Week3Day4 Homework #1.py
@@ -0,0 +1,9 @@
+grade = float(input('Enter Percent: '))
+if grade >= 90:
+    print('You Get a A!')
+elif grade >= 80:
+    print('You Get a B!')
+elif grade >= 70:
+    print('You Get a C!')
+else:
+    print('You Get NC!')
